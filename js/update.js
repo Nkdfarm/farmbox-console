02426a0c3ade8128182560ac146fe889c4e47a1b
Kissanga <gaudiosomail@gmail.com>
2026-09-26 12:55:43 +0200

    Console update

--- js/update.js
+++ js/update.js
@@ -14,7 +14,7 @@
 // ═══════════════════════════════════════════════════════════════════════════
 import { el, icon } from './ui.js';
 
-export const VERSION = '0.7.131';
+export const VERSION = '0.7.132';
 
 const DISMISSED = 'fbc_update_dismissed';
 const TARGET    = 'fbc_update_target';
